1beedd35cd82b813274417f803e6da8e4c7a47cc
Kary Rguez <practicasgithub@gmail.com>
2020-07-09 12:38:08 -0500

    Modificadores de acceso: Public y Private

---
+++ src/paquete1/Clase2.java
@@ -0,0 +1,9 @@
+package paquete1;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        Clase1 cl1 = new Clase1();
+        int c1 = cl1.variable1;
+    }
+
+}
---
+++ src/paquete2/Clase3.java
@@ -0,0 +1,9 @@
+package paquete2;
+import paquete1.Clase1;
+
+public class Clase3 {
+    public static void main(String[] args) {
+        Clase1 cl1 = new Clase1();
+        int clase1 = cl1.variable1;
+    }
+}
