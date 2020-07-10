a7a82e0ae74d9fe8229abc9402450843d690a5bd
Kary Rguez <practicasgithub@gmail.com>
2020-07-10 14:52:28 -0500

    Ejemplo While

---
+++ src/EjemploWhile.java
@@ -0,0 +1,10 @@
+public class EjemploWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        while ( i < 100) {
+            System.out.println("El valor de i es: " + i);
+            i++;
+        }
+        System.out.println("Terminó el ciclo While");
+    }
+}
