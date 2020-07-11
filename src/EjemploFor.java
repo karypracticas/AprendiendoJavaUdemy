ba04bf529856462d7d29fc6069c902061ca12971
Kary Rguez <practicasgithub@gmail.com>
2020-07-11 18:01:43 -0500

    Ejemplo ciclo for

---
+++ src/EjemploFor.java
@@ -0,0 +1,8 @@
+public class EjemploFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fin del ciclo for");
+    }
+}
