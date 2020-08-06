397031d55b3950bbfb3865438695b59518e57a23
Kary Rguez <practicasgithub@gmail.com>
2020-08-06 15:01:30 -0500

    Eventos: MouseEvent

---
+++ src/Eventos/EjemploMouseEvent.java
@@ -0,0 +1,78 @@
+package Eventos;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EjemploMouseEvent extends JFrame {
+
+        //Método constructor
+    public EjemploMouseEvent() {
+
+        //Crear elementos
+        JLabel lblSaludo = new JLabel();
+        lblSaludo.setBounds(25,50,200,100);
+        JTextField campo = new JTextField();
+        campo.setBounds(25,200,200,50);
+        JButton boton = new JButton("Haz Click");
+        boton.setBounds(25,300,200,50);
+
+
+
+
+        //Crear Ventana
+        JFrame marco = new JFrame();
+        marco.setSize(500,500);
+        marco.setLayout(null);
+        marco.setVisible(true);
+        marco.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Agregar elementos a la ventana
+        marco.add(lblSaludo);
+        marco.add(campo);
+        marco.add(boton);
+
+        MouseListener listener = new MouseListener() {
+
+            //Se implementan todos sus metodos porque es una interface
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                lblSaludo.setText("Mouse Clicked");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                lblSaludo.setText("Mouse Pressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                lblSaludo.setText("Mouse Released");
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                lblSaludo.setText("Mouse Entered");
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                lblSaludo.setText("Mouse Exited");
+
+            }
+        };
+
+        //Agregar evento al botón
+        boton.addMouseListener(listener);
+    }
+
+
+    public static void main(String[] args) {
+        //Instanciar clase
+        EjemploMouseEvent obj = new EjemploMouseEvent();
+
+
+    }
+}
