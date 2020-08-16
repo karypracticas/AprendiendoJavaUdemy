e9e15f3d105a8f86f5a5b4dac2a23a08b308deca
practicaskary2 <practicasgithub2@gmail.com>
2020-08-15 22:51:04 -0500

    Esto es un commit desde un branch

--- src/ClasePruebaCommit.java
+++ src/ClasePruebaCommit.java
@@ -2,5 +2,6 @@ public class ClasePruebaCommit {
 
     public static void main(String[] args) {
         System.out.println("Hello from cuenta2");
+        System.out.println("Commit desde branch ramaKary2");
     }
 }
